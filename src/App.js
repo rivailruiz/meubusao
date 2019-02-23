392dd971862fafd5e62606bc2682a41cbeca5ed8
rivailruiz <ruizalexander@id.uff.br>
2019-02-23 16:37:55 -0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-class App extends Component {
-  render() {
+
+  
+  class App extends Component {
+    state = {
+      veiculos: [],
+      mensagem: ''
+    }
+    componentDidMount(){
+      axios.get(`http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/7986/159`)
+      .then(res => {
+        console.log(res.data)
+        if(!res.data.length){
+          this.setState({mensagem: 'Sem ônibus vindo :('})
+        }else {
+          this.setState({mensagem: 'Ônibus a caminho:'})
+        }
+        const veiculos = res.data;
+        this.setState({ veiculos });
+      })
+    }
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="titulo">{this.state.mensagem}</h1>
+        <ul>
+          { this.state.veiculos.map(veiculo => <li><span>{veiculo.nameLine} em: </span>{veiculo.arrivalTime}min</li>)}
+        </ul>
       </div>
     );
   }
