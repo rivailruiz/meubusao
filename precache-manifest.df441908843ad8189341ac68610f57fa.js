dbac65d25beeadafc0855242ee0bb31a79f182b1
rivailruiz <ruizalexander@id.uff.br>
2019-02-23 16:38:44 -0300

    Updates

---
+++ precache-manifest.df441908843ad8189341ac68610f57fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meubusao/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9a8ef7b93b1a34ac038",
+    "url": "/meubusao/static/js/runtime~main.f9a8ef7b.js"
+  },
+  {
+    "revision": "c68aa14d6a04cf826e4a",
+    "url": "/meubusao/static/js/main.c68aa14d.chunk.js"
+  },
+  {
+    "revision": "66556e500ad1064ec3ea",
+    "url": "/meubusao/static/js/2.66556e50.chunk.js"
+  },
+  {
+    "revision": "c68aa14d6a04cf826e4a",
+    "url": "/meubusao/static/css/main.8abc0fed.chunk.css"
+  },
+  {
+    "revision": "ea067f5102802df8d96f7fb93cdab87e",
+    "url": "/meubusao/index.html"
+  }
+];
