0c1c4c9b0ac988a63d5caf9855aa6386b99206a0
rivailruiz <ruizalexander@id.uff.br>
2019-04-02 00:46:27 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -8,12 +8,40 @@ import './App.css';
   class App extends Component {
     state = {
       veiculos: [],
-      mensagem: ''
+      mensagem: '',
+      escondeLoading: false,
+      local: '',
+      localSelecionado: '7986'
     }
-    componentDidMount(){
-      axios.get(`http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/7986/159`)
+    getRoute = (local) => {
+      axios.get('https://cors-anywhere.herokuapp.com/http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/'+local+'/159')
       .then(res => {
+        // var img = document.getElementsByClassName('img')[];
+        // img.style.display = 'none';
         console.log(res.data)
+        this.setState({escondeLoading:true})
+        if(!res.data.length){
+          this.setState({mensagem: 'Sem ônibus vindo :('})
+        }else {
+          this.setState({mensagem: 'Ônibus a caminho:'})
+        }
+        const veiculos = res.data;
+        this.setState({ veiculos });
+      })
+    }
+    onChangeLocal = (e) => {
+      this.getRoute(e.target.value)
+    }
+
+    
+ 
+    componentDidMount(){
+      axios.get(`https://cors-anywhere.herokuapp.com/http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/7986/159`)
+      .then(res => {
+        // var img = document.getElementsByClassName('img')[];
+        // img.style.display = 'none';
+        console.log(res.data)
+        this.setState({escondeLoading:true})
         if(!res.data.length){
           this.setState({mensagem: 'Sem ônibus vindo :('})
         }else {
@@ -24,8 +52,17 @@ import './App.css';
       })
     }
     render() {
+      
     return (
       <div className="App">
+      {this.state.escondeLoading ? '' : <img src="loading.gif" className="img"/> }
+        <label>Onde você está?</label>
+        <select onChange={this.onChangeLocal} >
+          <option>Selecione uma opção</option>
+          <option value="7986">Associação de moradores Anaia</option>
+          <option value="7573">Passarela Arsenal (sentido Niterói)</option>
+          <option value="7556">Passarela Arsenal (sentido Anaia)</option>
+        </select>
         <h1 className="titulo">{this.state.mensagem}</h1>
         <ul>
           { this.state.veiculos.map(veiculo => <li><span>{veiculo.nameLine} em: </span>{veiculo.arrivalTime}min</li>)}
