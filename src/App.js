6a4eb75c5b67874d3f475cc279af6ec6008db801
rivailruiz <ruizalexander@id.uff.br>
2019-04-02 00:57:22 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -36,28 +36,28 @@ import './App.css';
     
  
     componentDidMount(){
-      axios.get(`https://cors-anywhere.herokuapp.com/http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/7986/159`)
-      .then(res => {
-        // var img = document.getElementsByClassName('img')[];
-        // img.style.display = 'none';
-        console.log(res.data)
-        this.setState({escondeLoading:true})
-        if(!res.data.length){
-          this.setState({mensagem: 'Sem ônibus vindo :('})
-        }else {
-          this.setState({mensagem: 'Ônibus a caminho:'})
-        }
-        const veiculos = res.data;
-        this.setState({ veiculos });
-      })
+      // axios.get(`https://cors-anywhere.herokuapp.com/http://zn4.m2mcontrol.com.br/api//forecast/lines/load/forecast/lines/fromPoint/7986/159`)
+      // .then(res => {
+      //   // var img = document.getElementsByClassName('img')[];
+      //   // img.style.display = 'none';
+      //   console.log(res.data)
+      //   if(!res.data.length){
+      //     this.setState({mensagem: 'Sem ônibus vindo :('})
+      //   }else {
+      //     this.setState({mensagem: 'Ônibus a caminho:'})
+      //   }
+      //   const veiculos = res.data;
+      //   this.setState({ veiculos });
+      // })
+      this.setState({escondeLoading:true})
     }
     render() {
       
     return (
       <div className="App">
       {this.state.escondeLoading ? '' : <img src="loading.gif" className="img"/> }
-        <label>Onde você está?</label>
-        <select onChange={this.onChangeLocal} >
+        <h4 className={this.state.escondeLoading ? '' : 'hide'}>Onde você está?</h4>
+        <select className={this.state.escondeLoading ? '' : 'hide'} onChange={this.onChangeLocal} >
           <option>Selecione uma opção</option>
           <option value="7986">Associação de moradores Anaia</option>
           <option value="7573">Passarela Arsenal (sentido Niterói)</option>
